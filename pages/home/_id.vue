340cc575b6866bfba74485f51bea10e27fc27e72
eclecticcoding <chuck@eclecticsaddlebag.com>
2022-04-01 09:43:08 -0400

    Set up basic routing

---
+++ pages/home/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ home.title }}</div>
+</template>
+
+<script>
+import homes from '@/data/homes.json';
+
+export default {
+  data(){
+    return {
+      home: {}
+    }
+  },
+  created() {
+    const home = homes.find(home => home.objectID === this.$route.params.id)
+    this.home = home
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
